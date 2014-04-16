daacd4c54120e2b952833b730b7a208ba43a5f22
smalls12 <cdriehuys@gmail.com>
2014-04-16 11:19:56 -0400

    Improved ImageSprite class.

--- Moving Platforms/src/ImageSprite.java
+++ Moving Platforms/src/ImageSprite.java
@@ -1,3 +1,4 @@
+import java.awt.Graphics;
 import java.awt.Image;
 import java.net.URL;
 
@@ -7,7 +8,6 @@ import javax.swing.ImageIcon;
 public class ImageSprite {
 	
 	private int xPos, yPos;
-	private int dx, dy;
 	
 	private Image img;
 	
@@ -15,8 +15,6 @@ public class ImageSprite {
 		
 		this.xPos = x;
 		this.yPos = y;
-		this.dx = 0;
-		this.dy = 0;
 		
 		if (url != null) {
 			this.img = new ImageIcon(url).getImage();
@@ -25,15 +23,17 @@ public class ImageSprite {
 		}
 	}
 	
+	public void draw(Graphics g) {
+		g.drawImage(this.img, this.xPos, this.yPos, this.getWidth(), this.getHeight(), null);
+	}
+	
 	public int getX() { return this.xPos; }
 	public void setX(int x) { this.xPos = x; }
 	
 	public int getY() { return this.yPos; }
 	public void setY(int y) { this.yPos = y; }
 	
-	public int getDX() { return this.dx; }
-	public void setDX(int dx) { this.dx = dx; }
+	public int getWidth() { return this.img.getWidth(null); }
 	
-	public int getDY() { return this.dy; }
-	public void setDY(int dy) { this.dy = dy; }
+	public int getHeight() { return this.img.getHeight(null); }
 }
