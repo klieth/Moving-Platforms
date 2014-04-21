53a98c2c31e92598c73af597195bd04b7dbd8d1b
smalls12 <cdriehuys@gmail.com>
2014-04-21 16:26:13 -0400

    Merged ImageSprite and Mob classes

--- Moving Platforms/src/Mob.java
+++ Moving Platforms/src/Mob.java
@@ -2,19 +2,44 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.net.URL;
 
+import javax.swing.ImageIcon;
 
-public class Mob extends ImageSprite {
+
+public class Mob {
 	
 	//this will be the base class for all moving objects (possibly barring the player)
+	private double xPos, yPos;
+	private double centerX, centerY;
+	private URL url;
+	
+	private double rotation;
+	
+	private Image img;
+	
 	int width, height;
-	double rotation;
+	
 	double vel;
 	double xVel, yVel;
-	Image sprite;
 
 	public Mob(int x, int y, String imgUrl) {
-		super(x, y, imgUrl);
 		
+		this.xPos = x;
+		this.yPos = y;
+		this.url = this.getClass().getResource(imgUrl);
+		
+		if (this.url != null) {
+			
+			this.img = new ImageIcon(this.url).getImage();
+			this.centerX = this.xPos + (getWidth() / 2);
+			this.centerY = this.yPos + (getHeight() / 2);
+		} else {
+			
+			this.img = null;
+			this.centerX = this.xPos;
+			this.centerY = this.yPos;
+		}
+		
+		this.rotation = 0;
 		this.xVel = 0;
 		this.yVel = 0;
 	}
@@ -37,6 +62,45 @@ public class Mob extends ImageSprite {
 		updateCenter();
 	}
 	
+	public void draw(Graphics2D g2d) {
+		
+		g2d.rotate(rotation, centerX, centerY);
+		g2d.drawImage(getImage(), (int) getX(), (int) getY(), getWidth(), getHeight(), null);
+		g2d.rotate(-rotation);
+	}
 	
+	public double getX() { return this.xPos; }
+	public void setX(double x) { this.xPos = x; }
+	
+	public double getY() { return this.yPos; }
+	public void setY(double y) { this.yPos = y; }
+	
+	public void setPosition(double x, double y) {
+		
+		this.xPos = x;
+		this.yPos = y;
+	}
+	
+	public int getWidth() { return this.img.getWidth(null); }
+	
+	public int getHeight() { return this.img.getHeight(null); }
+	
+	public double getRotation() { return this.rotation; }
+	public void setRotation(int rotation) { this.rotation = rotation; }
+	
+	public Image getImage() { return this.img; }
+	
+	public void updateCenter() {
+		
+		if (this.img != null) {
+			
+			this.centerX = this.xPos + (getWidth() / 2);
+			this.centerY = this.yPos + (getHeight() / 2);
+		} else {
+			
+			this.centerX = this.xPos;
+			this.centerY = this.yPos;
+		}
+	}
 	
 }
