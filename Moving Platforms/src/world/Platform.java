c4097a0efcb1802b2cef42d9fcf1284068ac91e1
smalls12 <cdriehuys@gmail.com>
2014-05-02 09:21:10 -0400

    Changed code to allow for bigger platform

--- Moving Platforms/src/world/Platform.java
+++ Moving Platforms/src/world/Platform.java
@@ -6,7 +6,7 @@ import core.SpriteSheet;
 
 public class Platform extends Mob{
 	
-	public final static int SHEET_WIDTH = 72;
+	public final static int SHEET_WIDTH = 100;
 	public final static int SHEET_HEIGHT = 9;
 	
 	public final static String SHEET_PATH = "/Sprites/Platform.png";
